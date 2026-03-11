843469485ffd74730dca0c801198c995181ed15f
MansaAlman <MSAlman@student.fullsail.edu>
2026-03-11 07:27:42 -0400

    Player HUD started
    Created a player hp bar, but the player does not yet have an hp field to fill the bar.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public GameObject player;
+    public Image playerHPBar;
+
     private void Awake()
     {
         instance = this;
+
+        player = GameObject.FindWithTag("Player");
     }
     void Start()
     {
