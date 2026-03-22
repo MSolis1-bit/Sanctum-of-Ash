94f72c44884b46b716f1b63d9154a4143d9d70f9
Michael Solis <masolis1@student.fullsail.edu>
2026-03-21 19:37:07 -0500

    player movement improved

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -395,8 +395,12 @@ public class PlayerController : MonoBehaviour, IDamage/*, IDataPersistence*/
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.fixedDeltaTime;
         }
-
-        // Makes short jumps feel better
+        // Makes full jumps less floaty even when jump is held
+        else if (rb.linearVelocity.y > 0f && Input.GetButton("Jump"))
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * 0.5f * Time.fixedDeltaTime;
+        }
+        // Makes short jumps feel tighter when jump is released early
         else if (rb.linearVelocity.y > 0f && !Input.GetButton("Jump"))
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.fixedDeltaTime;
